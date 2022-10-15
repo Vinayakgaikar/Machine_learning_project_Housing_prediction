53a75a1304244a920d3172e9120e61fd85856cf8
Vinayakgaikar <vinayakgaikar1998@gmail.com>
2022-10-15 21:18:43 +0530

    Modify app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting Machine learning Project flow with instructions"
+    return "Starting Machine learning Project flow with CI/CD pipeline"
 
 if __name__=="__main__":
     app.run(debug=True)
