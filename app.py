ccaa461796cdd422af5a02d88c5085af602e0963
Vinayakgaikar <vinayakgaikar1998@gmail.com>
2022-10-17 23:18:14 +0530

    Adding housing Exception and Logger to get error information

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from distutils.log import debug
+import sys
 import flask
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting Machine learning Project flow with CI/CD pipeline"
+    try:
+        raise Exception("we are testing custom exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
+    return "Starting Machine learning Project with CI/CD pipeline"
 
 if __name__=="__main__":
     app.run(debug=True)
