2d19b94cc6d7cea4c49890a1d00fb7232474880f
Vinayakgaikar <vinayakgaikar1998@gmail.com>
2022-10-23 23:49:47 +0530

    Update exception file

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -13,15 +13,19 @@ class HousingException(Exception):
     @staticmethod
     def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
         """
-        error_message : Exception object
-        error_detail : object of sys module
+        error_message: Exception object
+        error_detail: object of sys module
         """
-
-
         _,_ ,exec_tb = error_detail.exc_info()
-        line_number= exec_tb.tb_frame.f_lineno
-        file_name= exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occured in scrip: [{file_name}] at line number: [{line_number}] error message : [{error_message}]"
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
+        file_name = exec_tb.tb_frame.f_code.co_filename
+        error_message = f"""
+        Error occured in script: 
+        [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] 
+        error message: [{error_message}]
+        """
         return error_message
 
     # To get output given in print() statement.
